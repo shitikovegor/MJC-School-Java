a46a2ef5ac66d7cbc99de3247652c2b98f00fc6e
Egor Shitikov <shitikov.egor@gmail.com>
2021-02-10 16:38:55 +0300

    fix errors

--- gift_certificates_system/persistence/src/test/java/com/epam/esm/dao/configuration/PersistenceTestConfiguration.java
+++ gift_certificates_system/persistence/src/test/java/com/epam/esm/dao/configuration/PersistenceTestConfiguration.java
@@ -1,11 +1,13 @@
 package com.epam.esm.dao.configuration;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication(scanBasePackages = "com.epam.esm")
+@Configuration
+@ComponentScan("com.epam.esm")
 @EntityScan("com.epam.esm")
+@EnableAutoConfiguration
 public class PersistenceTestConfiguration {
 }
